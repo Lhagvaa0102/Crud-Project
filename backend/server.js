93049955c3df884656d3453136028ca2ef4bcbb0
Lhagvaa0102 <lhagvaa455@gmail.com>
2024-10-04 11:03:27 +0800

    Backend uurchlult oruulaw

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-  res.header(
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  response.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
