80f49c5c62372f868bd89dd2811b0edf4de780ad
Lhagvaa0102 <lhagvaa455@gmail.com>
2024-10-04 10:56:44 +0800

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/AddClothes", (request, response) => {
   const { name, list, price } = request.body;
